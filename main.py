10cfd854f691f718d31fb399c284bd00d10738d3
CARLOS JS BEZERRA <karlos.juliano123@gmail.com>
2024-06-08 00:46:17 -0300

    removendo a linha teste

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 from openpyxl import load_workbook
-#teste
+
 # Caminho para o arquivo Excel dentro da pasta Pasta001
 file_path = os.path.join('Pasta001', 'planilha001.xlsx')
 
