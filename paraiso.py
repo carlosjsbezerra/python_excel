780dd4162e7ee5359bfb85f10b248ea31809c76c
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2024-06-08 12:41:37 -0300

    Regionais add ARAGUATINS, ARRAIAS, COLINAS, DIANOPOLIS, GUARAI, GURUPI, MIRACEMA, PALMAS, PARAISO.

--- paraiso.py
+++ paraiso.py
@@ -7,11 +7,11 @@ print("Subplanilhas em nova_planilha.xlsx:", wb_escolas.sheetnames)
 # Obter a subplanilha "Escolas"
 ws_escolas = wb_escolas['Escolas']
 
-# Carregar a planilha "planilha002" e obter a subplanilha "SRE ARRAIAS"
+# Carregar a planilha "planilha002" e obter a subplanilha "SRE PARAISO"
 wb_planilha002 = load_workbook(filename='Pasta002/planilha002.xlsx')
 print("Subplanilhas em planilha002.xlsx:", wb_planilha002.sheetnames)
 
-ws_sre_arraias = wb_planilha002['SRE ARRAIAS']
+ws_sre_paraiso = wb_planilha002['SRE PARAISO']
 
 # Inicializa uma lista para armazenar as linhas que serão copiadas
 linhas_para_copiar = []
@@ -21,25 +21,25 @@ for row in ws_escolas.iter_rows(min_row=2, values_only=True):
     valor_coluna_c = row[2]  # Valor na coluna C
     # Imprimir o valor da coluna C para cada linha
     print("Valor na coluna C:", valor_coluna_c)
-    if valor_coluna_c == 'ARRAIAS':  # Verificar se o valor na coluna "C" é "ARRAIAS"
+    if valor_coluna_c == 'PARAISO':  # Verificar se o valor na coluna "C" é "PARAISO"
         linhas_para_copiar.append(row)
         print("Linha encontrada:", row)
 
 # Verificar se alguma linha foi encontrada
 if not linhas_para_copiar:
-    print("Nenhuma linha encontrada com o valor 'ARRAIAS' na coluna C.")
+    print("Nenhuma linha encontrada com o valor 'PARAISO' na coluna C.")
 
-# Adicionar as linhas filtradas começando na célula A10 da subplanilha "SRE ARRAIAS"
+# Adicionar as linhas filtradas começando na célula A11 da subplanilha "SRE PARAISO"
 start_row = 11
 for i, linha in enumerate(linhas_para_copiar):
     for j, value in enumerate(linha):
-        ws_sre_arraias.cell(row=start_row + i, column=j + 1, value=value)
+        ws_sre_paraiso.cell(row=start_row + i, column=j + 1, value=value)
 
 # Verificar se as linhas foram adicionadas
 if linhas_para_copiar:
-    print(f"{len(linhas_para_copiar)} linhas copiadas para 'SRE ARRAIAS'.")
+    print(f"{len(linhas_para_copiar)} linhas copiadas para 'SRE PARAISO'.")
 
 # Salvar as alterações na planilha "planilha002"
 wb_planilha002.save(filename='Pasta002/planilha002.xlsx')
 
-print("Linhas copiadas com sucesso para a subplanilha 'SRE ARRAIAS'.")
+print("Linhas copiadas com sucesso para a subplanilha 'SRE PARAISO'.")
